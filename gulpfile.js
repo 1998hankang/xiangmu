9d533f1c0030350db7757e2b0ceb4fdc5f22420c
1998hankang <1176893753@qq.com>
2018-06-02 09:38:18 +0800

    第三次传，主页已经到结尾

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,13 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 
 gulp.task('sass',function(){
-	gulp.src('sass/*.scss').pipe(sass()).pipe(rename({"suffix":".min"})).pipe(cssnano()).pipe(gulp.dest('css'));
+	gulp.src('src/scss/*.scss').pipe(sass()).pipe(rename({"suffix":".min"})).pipe(cssnano()).pipe(gulp.dest('css'));
 })
-
+//压缩js/重命名js/合并js
 gulp.task('js',function(){
-	gulp.src('src/js/*.js').pipe(uglify()).pipe(rename({"suffix":'.min'})).pipe(concat("all.js")).pipe(gulp.dest('js'));
+	gulp.src('src/js/*.js').pipe(uglify()).pipe(rename({"suffix":'.min'})).pipe(gulp.dest('js'));
 })
-
+//监听
 gulp.task('watch',function(){
 	gulp.watch(['src/js/*.js','src/scss/*.scss'],['js','sass']);
 })
-
